b8c7271a871d9b8faa3dd1957340037c178fdd0b
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-10-14 03:03:12 -0500

    Getting Tasks from Local Storage

--- ToDoListProject1/app.js
+++ ToDoListProject1/app.js
@@ -13,6 +13,9 @@ loadEventListeners();
 // Load all event listeners
 function loadEventListeners(){
 
+  // DOM Load Event
+  document.addEventListener("DOMContentLoaded",getTasks)
+
   // Add task event
   form.addEventListener('submit', addTask);
 
@@ -25,6 +28,44 @@ function loadEventListeners(){
   // Filter tasks event
   filter.addEventListener('keyup', filterTasks);
 }
+
+// GET Tasks from LS
+function getTasks(){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+  } else {
+    tasks=JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task){
+    // Create li element
+const li=document.createElement('li');
+
+// Add class
+li.className='collection-item';
+
+// Create text node and append to li
+li.appendChild(document.createTextNode(task));
+
+// Create a new link element
+const link=document.createElement('a');
+
+// Add class to it
+link.className='delete-item secondary-content';
+
+// Add icon html
+link.innerHTML= '<i class="fa fa-remove"></i>';
+
+// Append the link to li
+li.appendChild(link);
+
+// Append li to ul
+taskList.appendChild(li);  
+
+  });
+}
+
 
 // Add task
 function addTask(e){
