d17a59ae68c1020a1d19e98e840e57c77022e62a
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-10-14 01:48:49 -0500

    Filtering tasks event

--- ToDoListProject1/app.js
+++ ToDoListProject1/app.js
@@ -20,7 +20,10 @@ function loadEventListeners(){
   taskList.addEventListener('click', removeTask);
 
   //Clear task events
-  clearBtn.addEventListener('click',clearTasks)
+  clearBtn.addEventListener('click',clearTasks);
+
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -74,6 +77,21 @@ function removeTask(e){
 function clearTasks(){
 
   while(taskList.firstChild){
-    taskList.removeChild(taskList.firstChild)
+    taskList.removeChild(taskList.firstChild);
   }
+}
+
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
