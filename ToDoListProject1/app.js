74320ace65797b201d4a07afd35a79c17a7f12c1
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-10-14 02:30:34 -0500

    Persisting data to local storage, just for current session

--- ToDoListProject1/app.js
+++ ToDoListProject1/app.js
@@ -56,11 +56,28 @@ li.appendChild(link);
 // Append li to ul
 taskList.appendChild(li);
 
+// Store in LS
+storeTaskInLocalStorage(taskInput.value)
+
 // Clear input
 taskInput.value='';
 
   e.preventDefault();
 }
+
+// Store Task
+function storeTaskInLocalStorage(task){
+  let tasks;
+  if(localStorage.getItem('tasks')===null){
+    tasks=[];
+  } else {
+    tasks=JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
 
 // Remove Task
 function removeTask(e){
