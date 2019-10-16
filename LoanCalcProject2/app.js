243c938552cdd8e9110cf61eb4a79999c340f5ba
AbdlMalik Sharif <abdumaliksharipov@gmail.com>
2019-10-16 09:12:00 -0500

    Loan Calculator Project Initiation

---
+++ LoanCalcProject2/app.js
@@ -0,0 +1,16 @@
+// Listen for submit
+document.getElementById('loan-form').addEventListener('submit',calculateResults);
+
+
+
+
+
+
+
+
+
+
+// Calculate Results
+function calculateResults(e){
+  console.log('Loading ...');
+}
