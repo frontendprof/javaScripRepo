ab2c19da3f2c5a3ed903ba5eb74fd6491d94502b
AbdulMalik <abdumaliksharipov@gmail.com>
2020-04-21 08:06:47 -0500

    Push 2

--- rgbColorProject/app.js
+++ rgbColorProject/app.js
@@ -8,3 +8,26 @@ let colors=[
     "rgb(0,0,255)",
     "rgb(255,0,255)"
 ]
+
+
+let squares=document.querySelectorAll(".square");
+
+let pickedColor=colors[3];
+
+let colorDisplay=document.getElementById("colorDisplay");
+
+colorDisplay.textContent=pickedColor;
+
+for(let i=0;i<squares.length;i++){
+    squares[i].style.backgroundColor=colors[i];
+
+    squares[i].addEventListener("click",function(){
+        let clickedColor=this.style.background;
+        
+        if (clickedColor===pickedColor){
+            alert("Correct");
+        }else{
+            alert("Wrong");
+        }
+    });
+}
